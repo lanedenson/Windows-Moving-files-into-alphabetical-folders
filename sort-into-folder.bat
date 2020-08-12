73416fe424d31299084977f9a728081b432cbe1d
Lane Denson <lanedenson@users.noreply.github.com>
2020-08-12 15:39:50 -0500

    Update sort-into-folder.bat

--- sort-into-folder.bat
+++ sort-into-folder.bat
@@ -1,8 +1,12 @@
+rem Change the two .mp3 extensions to whatever file type you're working with.
+
 @ECHO ON
 
 SET Letters=(A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z)
 
 SET Numbers=(0,1,2,3,4,5,6,7,8,9)
+
+
 
 FOR %%A IN %Letters% DO ECHO Y | mkdir %%A | MOVE "%%~A*.mp3" "%%~A\"
 
